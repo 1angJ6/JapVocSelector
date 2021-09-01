aed29a10688e6d57d93a0eadf54a3e4300867db6
1ang <larrylang1209@gmail.com>
2021-09-01 10:54:19 +0800

    12
    12

--- jap-voc-selector-website/src/components/VocabularyList.js
+++ jap-voc-selector-website/src/components/VocabularyList.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Button, Dropdown, Menu } from 'antd';
+import { Divider, Dropdown, Menu } from 'antd';
+import { HeartOutlined } from "@ant-design/icons";
 import './VocabularyList.css';
-import { chinese_set_1 } from '../constant/chapter_1';
+import { chinese_set_1, jap_set_1 } from '../constant/chapter_1';
+import { chineseList, japList } from "../constant/vocabularyList";
 
 class VocabularyList extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            chinese_set: [],
-            jap_set: [],
+            chinese_set: chinese_set_1,
+            jap_set: jap_set_1,
             selectedIndexes: this.randomNums(10, 0, chinese_set_1.length),
-            selectedChapter: 1,
+            selectedChapter: 0,
         };
     }
 
@@ -31,29 +33,105 @@ class VocabularyList extends React.Component {
             while (this.isExist(arr, ran)) {
                 ran = Math.ceil(Math.random() * (max - min) + min);
             }
-            arr[i] = ran;
+            arr[i] = ran-1;
         }
         return arr;
     }
 
     updateIndexes = () => {
-        this.setState({selectedIndexes: this.randomNums(10, 0, chinese_set_1.length)});
+        this.setState({selectedIndexes: this.randomNums(10, 0, this.state.chinese_set.length)});
     };
+
+    updateVocList = (e) => {
+        this.setState({
+            chinese_set: chineseList[e.key],
+            jap_set: japList[e.key],
+            selectedIndexes: this.randomNums(10, 0, chineseList[e.key].length),
+            selectedChapter: e.key,
+        });
+    }
 
     render() {
 
+        const menu = (
+            <Menu onClick={this.updateVocList}>
+                <Menu.Item key="0" icon={<HeartOutlined />}>
+                    第1課
+                </Menu.Item>
+                <Menu.Item key="1" icon={<HeartOutlined />}>
+                    第2課
+                </Menu.Item>
+                <Menu.Item key="2" icon={<HeartOutlined />}>
+                    第3課
+                </Menu.Item>
+                <Menu.Item key="3" icon={<HeartOutlined />}>
+                    第4課
+                </Menu.Item>
+                <Menu.Item key="4" icon={<HeartOutlined />}>
+                    第5課
+                </Menu.Item>
+                <Menu.Item key="5" icon={<HeartOutlined />}>
+                    第6課
+                </Menu.Item>
+                <Menu.Item key="6" icon={<HeartOutlined />}>
+                    第7課
+                </Menu.Item>
+                <Menu.Item key="7" icon={<HeartOutlined />}>
+                    第8課
+                </Menu.Item>
+                <Menu.Item key="8" icon={<HeartOutlined />}>
+                    第9課
+                </Menu.Item>
+                <Menu.Item key="9" icon={<HeartOutlined />}>
+                    第10課
+                </Menu.Item>
+                <Menu.Item key="10" icon={<HeartOutlined />}>
+                    第11課
+                </Menu.Item>
+                <Menu.Item key="11" icon={<HeartOutlined />}>
+                    第12課
+                </Menu.Item>
+            </Menu>
+        );
+
         return (
             <div>
-                <Button type="primary" onClick={this.updateIndexes}>Update</Button>
-                {
-                    this.state.selectedIndexes.map(
-                        index => {
-                            return (
-                                <div>{chinese_set_1[index]}</div>
-                            );
-                        }
-                    )
-                }
+                <div>
+                    <Dropdown.Button onClick={this.updateIndexes} overlay={menu}>
+                        単語生成
+                    </Dropdown.Button>
+                </div>
+                <Divider orientation="left" plain>
+                    第{Number(this.state.selectedChapter)+1}課
+                </Divider>
+                <div>
+                    {
+                        this.state.selectedIndexes.map(
+                            index => {
+                                return (
+                                    <div>{this.state.chinese_set[index]}</div>
+                                );
+                            }
+                        )
+                    }
+                </div>
+                <Divider orientation="left" plain>
+                    答え
+                </Divider>
+                <div>
+                    {
+                        this.state.selectedIndexes.map(
+                            index => {
+                                return (
+                                    <div>
+                                        <span>{this.state.chinese_set[index]}</span>
+                                        <span className="answer">{this.state.jap_set[index]}</span>
+                                    </div>
+                                );
+                            }
+                        )
+                    }
+                </div>
             </div>
             
         );
--- jap-voc-selector-website/src/constant/vocabularyList.js
+++ jap-voc-selector-website/src/constant/vocabularyList.js
@@ -1,11 +1,44 @@
 import { chinese_set_1, jap_set_1 } from "./chapter_1";
+import { chinese_set_2, jap_set_2 } from "./chapter_2";
+import { chinese_set_3, jap_set_3 } from "./chapter_3";
+import { chinese_set_4, jap_set_4 } from "./chapter_4";
+import { chinese_set_5, jap_set_5 } from "./chapter_5";
+import { chinese_set_6, jap_set_6 } from "./chapter_6";
+import { chinese_set_7, jap_set_7 } from "./chapter_7";
+import { chinese_set_8, jap_set_8 } from "./chapter_8";
+import { chinese_set_9, jap_set_9 } from "./chapter_9";
+import { chinese_set_10, jap_set_10 } from "./chapter_10";
+import { chinese_set_11, jap_set_11 } from "./chapter_11";
+import { chinese_set_12, jap_set_12 } from "./chapter_12";
 
 var chineseList = [
-    chinese_set_1
+    chinese_set_1,
+    chinese_set_2,
+    chinese_set_3,
+    chinese_set_4,
+    chinese_set_5,
+    chinese_set_6,
+    chinese_set_7,
+    chinese_set_8,
+    chinese_set_9,
+    chinese_set_10,
+    chinese_set_11,
+    chinese_set_12,
 ];
 
 var japList = [
-    jap_set_1
+    jap_set_1,
+    jap_set_2,
+    jap_set_3,
+    jap_set_4,
+    jap_set_5,
+    jap_set_6,
+    jap_set_7,
+    jap_set_8,
+    jap_set_9,
+    jap_set_10,
+    jap_set_11,
+    jap_set_12,
 ];
 
 export { chineseList, japList };
